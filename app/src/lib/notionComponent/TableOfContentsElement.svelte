4e312568be8ce156f765a26542ea0be237a3d69b
Henri Larget <1727893+decima@users.noreply.github.com>
2022-10-23 23:54:20 +0200

    Fix: toc behavior similar to notion (#6)
    * fix: same behavior between notion and notionslider about toc
    
    * handling multiple columns like notion

--- app/src/lib/notionComponent/TableOfContentsElement.svelte
+++ app/src/lib/notionComponent/TableOfContentsElement.svelte
@@ -2,7 +2,7 @@
     import RichText from "../notionPrimaries/RichText.svelte";
 
     export let items = [];
-    export let depth=3;
+    export let depth = 3;
 </script>
 
 <ul>
@@ -10,22 +10,28 @@
     {#each items as item }
         <li>
             <div class="{item.type}">
-            <RichText richText={item.content.rich_text}/>
+                <RichText richText={item.content.rich_text}/>
             </div>
-            {#if item.children.length >0}
-            <div class="pl-4">
-            <svelte:self items={item.children} depth={depth-1}/>
-            </div>
-                {/if}
+            {#if item.children.length > 0}
+                <!-- should never happen -->
+                <div class="pl-4">
+                    <svelte:self items={item.children} depth={depth-1}/>
+                </div>
+            {/if}
         </li>
     {/each}
 </ul>
 <style>
 
-    .heading_1{
-      @apply font-bold;
+    .heading_1 {
+        @apply font-bold;
     }
-    .heading_3{
+    .heading_2{
+        @apply pl-4;
+    }
+
+    .heading_3 {
+        @apply pl-8;
         @apply text-xs;
 
     }
